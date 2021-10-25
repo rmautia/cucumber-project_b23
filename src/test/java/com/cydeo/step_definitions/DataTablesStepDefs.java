3baa5128b7574569018d14dc489fdbce12c6811d
rmautia <raphaelnyangenya@gmail.com>
2021-10-26 00:44:36 +0300

    Created datatable.feature

---
+++ src/test/java/com/cydeo/step_definitions/DataTablesStepDefs.java
@@ -0,0 +1,34 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+
+public class DataTablesStepDefs {
+
+    @Given("I have a {string}")
+    public void i_have_a(String animal) {
+        System.out.println("Given I have a "+animal);
+    }
+    @When("I call their names")
+    public void i_call_their_names() {
+        System.out.println("When I call their name");
+    }
+    @Then("They come to me.")
+    public void they_come_to_me() {
+        System.out.println("Then they come to me");
+    }
+
+
+    @Given("I have the following animals")
+    public void i_have_the_following_animals(List<String> animalList) {
+
+        System.out.println("animalList = " + animalList);
+
+    }
+
+
+}
+
---
+++ src/test/resources/features/datatables.feature
@@ -0,0 +1,24 @@
+Feature: Demonstrating the data table feature for cucumber
+
+  Cucumber provide ability to organize repeating steps with different data
+  using the data table feature and automatically convert the table into
+  few known data types supported by cucumber
+
+  Scenario: Petting zoo
+    Given I have a "horse"
+    And I have a "dog"
+    And I have a "turtle"
+    And I have a "zebra"
+    When I call their names
+    Then They come to me.
+
+
+  Scenario: Petting zoo with table
+    Given I have the following animals
+#  if you want it to nicely align, control + alt + l
+      | horse  |
+      | dog    |
+      | turtle |
+      | zebra  |
+    When I call their names
+    Then They come to me.
