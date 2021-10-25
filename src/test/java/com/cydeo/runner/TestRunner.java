e4f67c7c60f8b639418b7d514ac1af3bf36ebe97
rmautia <raphaelnyangenya@gmail.com>
2021-10-25 23:57:18 +0300

    add .gitignore file

--- src/test/java/com/cydeo/runner/TestRunner.java
+++ src/test/java/com/cydeo/runner/TestRunner.java
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
                   glue = "com/cydeo/step_definitions",
-                  plugin = {"html:target/cucumber_report.html"},
+                  plugin = { "pretty","html:target/cucumber_report.html","json" },
                   dryRun = false
 //                    tags = "@wip"
                   )
